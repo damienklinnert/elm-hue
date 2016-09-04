bc5701c44f04a77fa2c855e4b7504836ca4e8cc0
Michael Torres <michael@notion.ai>
2016-09-04 15:57:18 -0400

    add decoding unit tests

--- tests/src/Tests/Lights.elm
+++ tests/src/Tests/Lights.elm
@@ -10,29 +10,60 @@ tests : Test
 tests =
     suite "Lights decoding"
         [ getAllLightsTest
+        , getLightStateTest
+        , updateLightResponseTest
         ]
+
+testDecoder : JD.Decoder a -> String -> Assertion
+testDecoder decoder str =
+    case JD.decodeString decoder str of
+        Ok _ ->
+            pass
+
+        Err e ->
+            fail e
 
 
 getAllLightsTest : Test
 getAllLightsTest =
-    let
-        testDecoder decoder str =
-            case JD.decodeString decoder str of
-                Ok _ ->
-                    pass
+    suite "Get all lights"
+        [ test "Version 1.4 with multiple lights"
+            <| testDecoder detailsListResponseDecoder getAllLights__1_4
+        , test "Version 1.7 with multiple lights"
+            <| testDecoder detailsListResponseDecoder getAllLights__1_7
+        , test "Version 1.9 with multiple lights"
+            <| testDecoder detailsListResponseDecoder getAllLights__1_9
+        , test "Version 1.11 with multiple lights"
+            <| testDecoder detailsListResponseDecoder getAllLights__1_11
+        , test "No lights available"
+            <| testDecoder detailsListResponseDecoder noLights
+        , test "Authentication error when fetching all lights"
+            <| testDecoder detailsListResponseDecoder invalidAuthError
+        ]
 
-                Err e ->
-                    fail e
-    in
-        suite "Get all lights"
-            [ test "Version 1.4 with multiple lights"
-                <| testDecoder detailsListDecoder getAllLights__1_4
-            , test "Version 1.7 with multiple lights"
-                <| testDecoder detailsListDecoder getAllLights__1_7
-            , test "Version 1.9 with multiple lights"
-                <| testDecoder detailsListDecoder getAllLights__1_9
-            , test "Version 1.11 with multiple lights"
-                <| testDecoder detailsListDecoder getAllLights__1_11
-            , test "No lights available"
-                <| testDecoder detailsListDecoder noLights
-            ]
+getLightStateTest : Test
+getLightStateTest =
+    suite "Get Light state"
+        [ test "Version 1.4"
+            <| testDecoder stateResponseDecoder lightState__1_4
+        , test "Version 1.11"
+            <| testDecoder stateResponseDecoder lightState__1_11
+        , test "Auth error"
+            <| testDecoder stateResponseDecoder invalidAuthError
+        ]
+
+
+updateLightResponseTest : Test
+updateLightResponseTest =
+    suite "Parse light update response"
+        [ test "Multi error and success response"
+            <| testDecoder multiResponse multiSuccessAndErrorLightUpdateResponse
+        , test "Single successful response"
+            <| testDecoder multiResponse singleSuccessLightUpdateResponse
+        , test "Multiple successful responses"
+            <| testDecoder multiResponse multiSuccessLightUpdateResponse
+        , test "All success value types"
+            <| testDecoder multiResponse allSuccessLightUpdateResponseTypes
+        , test "Single error response"
+            <| testDecoder multiResponse invalidAuthError
+        ]
